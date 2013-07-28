cf4048bc9ac38e00e3a2ceb3f4cae308a5663f80
ytbryan <ytbryan@gmail.com>
2013-07-28 22:25:41 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Briter::Application.config.secret_token = '1e976e9547868ae96071197bd6d9b106afb174f5a38356023a62b4f612847cf4b471e951d1aef132c644d21b33194cd71b5dcd4cad4bd3b59968d9da2a044903'
